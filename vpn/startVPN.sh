20ae257f9437ea1efd363711f595af223626d8d4
adityawasudeo <adityawasudeo@gmail.com>
2021-04-12 13:18:50 +0800

    Initial commit of vpn container configs

---
+++ vpn/startVPN.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+
+#iptables -t nat -A POSTROUTING -s 10.250.0.0/24 -o eth0 -j MASQUERADE
+
+iptables -I FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -m conntrack --ctstate NEW -j ACCEPT
+iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.250.0.0/24 -o eth0 -j MASQUERADE
+
+openvpn --config /server.conf
